218992121e97a2842124163e876a907dfa8008c4
Rafik Shukralla <rafik.shukralla@student.csulb.edu>
2024-09-28 13:09:11 -0700

    added firebase configuration

---
+++ firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtsSarzycqlYY8ZGBEYd2wsRINiGRO0xI",
+  authDomain: "dish-dash-11514.firebaseapp.com",
+  projectId: "dish-dash-11514",
+  storageBucket: "dish-dash-11514.appspot.com",
+  messagingSenderId: "318398720224",
+  appId: "1:318398720224:web:9ef380f871d54753340885",
+  measurementId: "G-ZFV88NTHY5"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+let analytics;
+
+if (typeof window !== 'undefined') {
+  // Client-side code
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+const db = getFirestore(app)
+export {db, analytics}
